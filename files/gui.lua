559262402fe5a2a7ccb5aca093a86d470385e963
Mir Sole <zburn101@gmail.com>
2025-01-07 12:47:08 +1100

    idk how gui works

--- files/gui.lua
+++ files/gui.lua
@@ -14,10 +14,21 @@ function OnWorldPostUpdate()
     end
 end
 
+
 function GuiRender()
     local gui = GuiCreate()
-    
     GuiStartFrame(gui)
+
+    local screen_width, screen_height = GuiGetScreenDimensions(gui)
+
+    if GuiButton(gui, 189032128, (screen_width / 3) * 1.8, (screen_height / 9) * 8.5, "[+]", 1) then
+        GlobalsSetValue("trinkets.gui_open", tostring(not tobool(GlobalsGetValue("trinkets.gui_open", "false"))))
+        GamePrint("yes")
+    end
+
+    if tobool(GlobalsGetValue("trinkets.gui_open", "false")) then
+        GamePrint("hi")
+    end
 
     GuiDestroy(gui)
 end
